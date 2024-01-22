55cfa5f71d6ddcf5644bef79bb9d419810400863
ahmedmellouli <ahmed.mellouli@esprit.tn>
2024-01-22 11:31:13 -0800

    user auth

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,26 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const routerUser =  require("./routes/users");
+const routerAuth =  require("./routes/auth");
 
 dotenv.config();
+// mongoose.connect(process.env.MONGO_URL).then(()=>console.log("connected to db")).catch(
+//     ()=>console.error("error")
+// )
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api/users",routerUser);
+app.use("/api/auth",routerAuth);
+
+
 
 app.listen(8800, () =>{
     console.log("Backend server is running!");
---
+++ backend/routes/users.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+router.get("/",(req,res) =>{
+    res.send("hey it's user router")
+})
+
+//update user
+router.put("/:id", async (req, res) =>{
+    if(req.body.userId === req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            }catch(err){
+                return res.status(500).json(err);
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id,{
+                $set:req.body,
+            });
+            res.status(200).json("Account has been updated")
+        }catch(err){
+            return res.status(500).json(err);
+        }
+    }else{
+        res.status(403).json("you can update only your account!");
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=router;
+
+
