e13c74cec46bbe58708d41729d620f9acd2111bc
ahmedmellouli <ahmed.mellouli@esprit.tn>
2024-01-24 15:03:46 -0800

    follow update

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,7 +8,7 @@ router.get("/",(req,res) =>{
 
 //update user
 router.put("/:id", async (req, res) =>{
-    if(req.body.userId === req.params.id || req.user.isAdmin){
+    if(req.body.userId === req.params.id || req.body.isAdmin){
         if(req.body.password){
             try{
                 const salt = await bcrypt.genSalt(10);
@@ -30,6 +30,53 @@ router.put("/:id", async (req, res) =>{
     }
 })
 
+
+//delete user
+router.delete("/:id", async (req, res) =>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        try{
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account has been deleted")
+        }catch(err){
+            return res.status(500).json(err);
+        }
+    }else{
+        res.status(403).json("you can delete only your account!");
+    }
+})
+
+//find user 
+router.get("/:id", async (req, res) =>{
+    try{
+        const user = await User.findById(req.params.id);
+        const {password,updatedAt, ...other} = user._doc;
+        res.status(200).json(other)
+    }catch{
+        res.status(500).json(err);
+    }
+})
+
+//follow
+router.put("/:id/follow", async (req,res) =>{
+    if(req.body.userId !==req.params.id){
+        try{
+            const currentUser = await User.findById(req.body.userId);
+            const user = await User.findById(req.params.id);
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({$push:{followers : req.body.userId}});
+                await currentUser.updateOne({$push:{followins: req.params.id}});
+                res.status(200).json("user has been followed");
+            }else{
+                res.status(403).json("you already follow this user");
+            }
+        }catch(err){
+            res.status(500).json(err);
+        }
+
+    }else{
+        res.status(403).json("you can't follow yourself");  
+    }
+})
 
 
 
